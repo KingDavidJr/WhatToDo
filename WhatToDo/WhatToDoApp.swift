4976f23ea7ab249a9f7dcb0486cb79f24e214c51
David <realkd872@gmail.com>
2022-10-04 09:00:06 -0500

    Initial Commit

---
+++ WhatToDo/WhatToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatToDoApp.swift
+//  WhatToDo
+//
+//  Created by David Amedeka on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct WhatToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
